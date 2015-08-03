7c4b11099eaf5fb474f8a839f5a46f4e13a48efc
Itay Shtekel <shtekel1@gmail.com>
2015-08-03 14:09:51 +0300

    added umd with grunt

---
+++ Gruntfile.js
@@ -0,0 +1,27 @@
+/**
+ * Created by itaysh on 8/3/15.
+ */
+
+'use strict';
+
+module.exports = function (grunt) {
+
+    grunt.initConfig({
+        umd: {
+            all: {
+                options: {
+                    src: 'DragDropMixin.js',
+                    dest: 'dist/DragDropMixin.js',
+                    objectToExport: 'DragDropMixin',
+                    amdModuleId: 'DragDropMixin',
+                    deps: {
+                        'default': ['lodash.includes']
+                    }
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-umd');
+
+};
---
+++ dist/DragDropMixin.js
@@ -0,0 +1,111 @@
+(function (root, factory) {
+  if (typeof define === 'function' && define.amd) {
+    // AMD. Register as an anonymous module unless amdModuleId is set
+    define('DragDropMixin', ["lodash.includes"], function (a0) {
+      return (root['DragDropMixin'] = factory(a0));
+    });
+  } else if (typeof exports === 'object') {
+    // Node. Does not work with strict CommonJS, but
+    // only CommonJS-like environments that support module.exports,
+    // like Node.
+    module.exports = factory(require("lodash.includes"));
+  } else {
+    root['DragDropMixin'] = factory(lodash.includes);
+  }
+}(this, function (lodash.includes) {
+
+'use strict';
+
+var includes = require("lodash.includes");
+
+var DragDropMixin = {
+    /*
+     *  usage:
+     *
+     *  mixins: [DragDropMixin],
+     *  dragDrop: function () {
+     *
+     *     return {
+     *
+     *         // when dragging an item
+     *         draggable: true,
+     *         dropType: 'myItem',
+     *         dataTransfer: { myItemData: property }
+     *
+     *         // when dropping an item:
+     *         droppable: true,
+     *         acceptableDrops: ['myItem'],
+     *         drop: function (myItem) {},
+     *     };
+     *  }
+     *
+     */
+    isAttrEnabled: function (attr) {
+        return this.dragDropData && this.dragDropData[attr];
+    },
+    isDroppable: function () {
+        return this.isAttrEnabled('droppable');
+    },
+    isDraggable: function () {
+        return this.isAttrEnabled('draggable');
+    },
+    componentDidMount: function () {
+        var node = this.getDOMNode();
+
+        this.dragDropData = this.dragDrop();
+
+        if (this.isDroppable()) {
+            node.addEventListener('dragover', this.handleDragOver, this);
+            node.addEventListener('drop', this.handleDrop, this);
+        }
+
+        if (this.isDraggable()) {
+            node.draggable = true;
+            node.addEventListener('dragstart', this.handleDragStart, this);
+        }
+    },
+    componentWillUnmount: function () {
+        var node = this.getDOMNode();
+
+        if (this.isDroppable()) {
+            node.removeEventListener('dragover', this.handleDragOver);
+            node.removeEventListener('drop', this.handleDrop);
+        }
+
+        if (this.isDraggable()) {
+            node.removeEventListener('dragstart', this.handleDragStart);
+        }
+    },
+    handleDragOver: function (e) {
+        e.preventDefault();
+    },
+    handleDrop: function (e) {
+        var jsonData = e.dataTransfer.getData('objToPass'),
+            passedObj = JSON.parse(jsonData),
+            acceptableDrops = this.dragDropData.acceptableDrops;
+
+        e.preventDefault();
+
+        if (!this.dragDropData.drop) {
+            throw new Error('Must define drop function when using droppable');
+        }
+
+        if (includes(acceptableDrops, passedObj.dropType)) {
+            this.dragDropData.drop(passedObj.data);
+        }
+
+    },
+    handleDragStart: function (e) {
+        var objToPass = {
+            data: this.dragDropData.dataTransfer,
+            dropType: this.dragDropData.dropType
+        };
+
+        e.dataTransfer.setData('objToPass', JSON.stringify(objToPass));
+    }
+};
+
+module.exports = DragDropMixin;
+return DragDropMixin;
+
+}));
