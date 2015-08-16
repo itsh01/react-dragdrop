f65329782a3b48c0f3e9e63823ac29f22fa8470f
Itay Shtekel <shtekel1@gmail.com>
2015-08-16 11:14:27 +0300

    testing drag and drop failing

--- spec/DragDropMixin.spec.js
+++ spec/DragDropMixin.spec.js
@@ -5,15 +5,25 @@
 var DragDropMixin = require('../dist/DragDropMixin');
 var React = require('react/addons');
 var TestUtils = React.addons.TestUtils;
+//var jsdom = require('jsdom');
 
 describe('DragDropMixin', function () {
 
     var element = null,
-        elementDrag = null;
+        elementDrag = null,
+        elementDrop = null;
 
     beforeEach(function () {
-        global.document = require('jsdom').jsdom();
-        global.window = global.document.defaultView;
+        //var html = '<html><body></body></html>';
+        //global.document = jsdom.jsdom(html);
+        //global.window = document.parentWindow;
+        //document.querySelector('body').addEventListener('click', function() {
+        //    console.log('clicked');
+        //});
+
+        //console.log(window);
+
+        //spyOn(DragDropMixin, 'handleDrop').and.callThrough();
 
         var Comp = React.createClass({
             mixins: [DragDropMixin],
@@ -38,8 +48,27 @@ describe('DragDropMixin', function () {
             }
         });
 
-        element = React.createElement(Comp,{});
-        elementDrag = React.createElement(CompDrag,{});
+        var CompDrop = React.createClass({
+            mixins: [DragDropMixin],
+            dragDrop: function dragDrop() {
+                var self = this;
+                return {
+                    droppable: true,
+                    acceptableTypes: ['test'],
+                    drop: function (data) {
+                        console.log(123);
+                        self.setState(data);
+                    }
+                };
+            },
+            render: function render() {
+                return React.createElement('div', {});
+            }
+        });
+
+        element = React.createElement(Comp, {});
+        elementDrag = React.createElement(CompDrag, {});
+        elementDrop = React.createElement(CompDrop, {});
 
     });
 
@@ -55,6 +84,39 @@ describe('DragDropMixin', function () {
         var rendered = TestUtils.renderIntoDocument(elementDrag);
 
         expect(rendered.getDOMNode().draggable).toBe(true);
+    });
+
+    it('should attach drop functionality when configured', function () {
+        var renderedDrag = TestUtils.renderIntoDocument(elementDrag);
+        var renderedDrop = TestUtils.renderIntoDocument(elementDrop);
+        var nodeDrag = renderedDrag.getDOMNode();
+        var nodeDrop = renderedDrop.getDOMNode();
+        var mockEvent = {
+            //nativeEvent: {
+                dataTransfer: {
+                    types: ["objtopass"],
+                    setData: function () {},
+                    getData: function () {
+                        return JSON.parse({
+                            dropType: 'test',
+                            data: {
+                                test: true
+                            }
+                        });
+                    }
+                }
+            //}
+        };
+
+        //spyOn(rendered.dragDropData, 'drop').and.callThrough();
+
+        TestUtils.SimulateNative.dragStart(nodeDrag, mockEvent);
+        TestUtils.Simulate.dragOver(nodeDrop, mockEvent);
+        TestUtils.Simulate.drop(nodeDrop, mockEvent);
+
+        //expect(DragDropMixin.handleDrop).toHaveBeenCalled();
+        //expect(rendered.dragDropData.drop).toHaveBeenCalled();
+        expect(renderedDrop.state).not.toBeNull();
     });
 
 });
